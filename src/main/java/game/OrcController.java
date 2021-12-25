f39abba5e587ad86a7352ece0dc1683910762244
Devyani <devyani20055@iiitd.ac.in>
2021-12-25 21:50:21 +0530

    changes

--- src/main/java/game/OrcController.java
+++ src/main/java/game/OrcController.java
@@ -14,6 +14,7 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class OrcController implements Initializable {
     @FXML
     private ImageView bossOrc;
