4f1e90d15a4476a78b9fbd241afa0f73afdd743a
Tarushi Gandhi <77500734+T-Gandhi@users.noreply.github.com>
2021-12-28 13:19:36 +0530

    orcs

--- src/main/java/game/TreasureChest.java
+++ src/main/java/game/TreasureChest.java
@@ -1,19 +1,16 @@
 package game;
 
-import javafx.scene.Node;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.lang.Integer;
 import java.util.HashMap;
 
-public class treasureChest extends Chests{
+public class TreasureChest extends Chests{
     private int id;
     private int coins;
 
     public HashMap<Integer,Integer> coinChest = new HashMap<>();
 
-    public treasureChest() throws IOException {
+    public TreasureChest() throws IOException {
     }
     public void setCoins(int coinCount){
         this.coins= coinCount;
