1f695e4d4034f5abe572f2e14a04eaec32e2f243
Devyani <devyani20055@iiitd.ac.in>
2022-01-02 23:15:43 +0530

    changes Chests

---
+++ src/main/java/game/WonController.java
@@ -0,0 +1,30 @@
+package game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WonController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+
+    public void ReturnToMain(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("WelcomeScreen.fxml"));
+        root =loader.load();
+        stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
